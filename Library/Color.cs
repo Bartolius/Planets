4a47e4f42d8e9b8851027e4c908c0e83815822a6
Bartolius <bartoszmajkut3@gmail.com>
2023-02-18 21:09:33 +0100

    Created class Color

---
+++ Library/Color.cs
@@ -0,0 +1,27 @@
+﻿namespace Library
+{
+    public class Color
+    {
+        private byte R { get; set; }
+        private byte G { get; set; }
+        private byte B { get; set; }
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public static string ForegroundColor(Color color)
+        {
+            return $"\x1b[38;2;{color.R};{color.G};{color.B}m";
+        }
+        public static string BackgroundColor(Color color)
+        {
+            return $"\x1b[48;2;{color.R};{color.G};{color.B}m";
+        }
+
+        public const string CLEAR = "\x1b[0m";
+    }
+}
